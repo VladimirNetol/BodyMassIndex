1a5eb78c4551586928a93346d9e7f4b8f313f877
Vladimir <vladimir_net0logia@mail.ru>
2025-07-11 19:35:11 +0300

    Cheange BMI2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double weightInKg, double lengthInMetres1) {
+    public int calculate(int weightInKg, double lengthInMetres1) {
         double index;
         index = weightInKg / (lengthInMetres1 * 2);
         return (int) index;
